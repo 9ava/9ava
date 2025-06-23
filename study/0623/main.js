a97960be5ee4152557a821e68a21382e45c107f8
9ava <zudrion@gmail.com>
2025-06-23 12:54:21 +0900

    0623 오전끝

---
+++ study/0623/main.js
@@ -0,0 +1,16 @@
+// alert("Hello, World! This is a JavaScript alert.");
+// console.log("Hello, World! This is a message in the console.");
+// document.write("<h1>Document Write</h1>");
+
+// let currentYear = 2025;
+// let birthYear = Number(prompt("태어난 해를 입력하세요"));
+// let age = currentYear - birthYear + 1;
+// alert("당신의 나이는 " + age + "살입니다.");
+
+let name = "철수";
+let age = "20";
+let isStudent = true;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
