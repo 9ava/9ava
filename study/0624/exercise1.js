2d9d67114cf730e0ee167f5d56ddf15e7f9f7b35
9ava <zudrion@gmail.com>
2025-06-24 12:53:12 +0900

    0624 오전

---
+++ study/0624/exercise1.js
@@ -0,0 +1,5 @@
+let age = Numver(prompt("나이를 입력해주세요"));
+if (age >19) {
+    alert("성인입니다.")
+}
+else {alert ("미성년자입니다.")}
---
+++ study/0624/exercise2.js
@@ -0,0 +1,23 @@
+// let sum = 0;
+// while(i <= 100){
+//     if (i %2 === 0){
+//         sum += i
+//     };
+//     i ++;
+//     console.log("1부터 100까지 짝수의 합", sum);
+
+// }
+
+// let sum = 0;
+// for (let i =1; i <101; i++) {
+//     if (i %2 +++0) sum += i;
+//     console.log ("sum")
+// }
+
+let num = prompt("숫자를 입력하세요.");
+while (num !== "exit")
+    {num = $(prompt) ; 
+console.log (`입력한 값 : ${num}`);
+
+
+}
