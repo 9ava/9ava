b68f6d6c6406a88ff10e8a6b799ed37f5d36aa44
9ava <zudrion@gmail.com>
2025-07-15 17:13:23 +0900

    0715 수업끝

--- study/0715/ch04/ch04_1/src/App.tsx
+++ study/0715/ch04/ch04_1/src/App.tsx
@@ -1,8 +1,44 @@
-// import {useClock} from './hooks'
+import { useClock } from "./hooks";
+import "./App.css";
+import { useEffect, useRef, useState } from "react";
 import Clock from "./pages/Clock";
 
+// export default function App() {
+// const [today, setToday] = useState(new Date());
+// let today = useRef(new Date());
+// useEffect(() => {
+//   console.log(`useEffect called.`);
+//   const duration = 1000;
+//   const id = setInterval(() => {
+//     setToday(new Date());
+// today.current = new Date();
+//   console.log(`today.current`, today.current.toLocaleTimeString());
+//   }, duration);
+//   return () => clearInterval(id);
+// }, []);
+
 export default function App() {
-  let today = new Date();
-  // const today = useClock()
-  return <Clock today={today} />;
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+
+  return (
+    <div>
+      <h2>{"덧셈기"}</h2>
+      <input
+        type="number"
+        value={number1}
+        onChange={(e) => setNumber1(parseInt(e.target.value))}
+      />
+      <span>{"+"}</span>
+      <input
+        type="number"
+        value={number2}
+        onChange={(e) => setNumber2(parseInt(e.target.value))}
+      />
+
+      <p>{`덧셈 결과는 : ${number1} + ${number2} = ${number1 + number2}`}</p>
+    </div>
+  );
+  // const today = useClock();
+  // return <Clock today={today} />;
 }
